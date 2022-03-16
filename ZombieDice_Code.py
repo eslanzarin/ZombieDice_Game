6ccc1c784323377a7a2b0fbf0e124c02201902ed
eslanzarin <eslanzarin@gmail.com>
2022-03-16 09:55:44 -0300

    Modifies ZombieDice code

--- ZombieDice_Code.py
+++ ZombieDice_Code.py
@@ -2,58 +2,108 @@
 # Please, read "README" file for rules on how to play and extra details about the project
 
 # import libraries
-
 import random  # generates random numbers or letters
 import time  # for small pauses between rounds and text
 
-# define variables (this is still something I've got to think about carefully)
-
+# define variables that will be used
 rounds = 0
+brains = 0
+hearts = 3
+steps = 0
+B = 'brain'
+G = 'gunshot'
+S = 'steps'
+
+# defines the lists to be filled
 players = []
+diceChoice = []
+side = []
 
-# code sorts 3 values from the list, then sorts the specific letter from each item
+# defines the sides of each dice
+GreenDice = 'BSBGSB'
+YellowDice = 'GSBGSB'
+RedDice = 'GSGBSG'
 
-cup = ['BSBGSB','BSBGSB','BSBGSB','BSBGSB','BSBGSB','BSBGSB','GSBGSB','GSBGSB','GSBGSB','GSBGSB','GSGBSG','GSGBSG','GSGBSG']
-
-GreenDice = "BSBGSB"
-YellowDice = "GSBGSB"
-RedDice = "GSGBSG"
+# defines the list of dice to be sorted from the cup/tube
+cup = ['GreenDice', 'GreenDice', 'GreenDice', 'GreenDice', 'GreenDice', 'GreenDice', 'YellowDice', 'YellowDice',
+       'YellowDice', 'YellowDice', 'RedDice', 'RedDice', 'RedDice']
 
 # greetings for the players
 print("WELCOME, ZOMBIES! ARE YOU READY FOR THE MOST THRILLING ADVENTURE OF ALL?")  # shows this message
 time.sleep(1)  # waits 1 sec for the next message
 
+# creates variable for number of players
 nPlayers = int(input("How many zombies will be joining us today?"))
 
+# conditions for the game to start
 if nPlayers < 2:
     print("We need at least 2 players to continue!")
 
 elif nPlayers > 8:
     print("Sorry, too many zombies in this game! We accept up to 8 players:")
 
+# nPlayers >= 2 or nPlayers <= 8
 else:
-    print("Alright! Let's start!")
+    print("Alright! Let's meet our players!")
+    time.sleep(1)
 
-    name = str(input("Please, press ENTER and type the names of the zombies separated by ENTER:"))  # identify and store the name of the players (how to make pressing enter not needed?
+# identify and store the name of the players
+    name = str(input("Please, press ENTER and type the names of the zombies separated by ENTER:"))
 
-    i = 0
-
+# stores players' names in the list 'players'
     for i in range(nPlayers):
-        players.append(input())  # stores players' names in a list
+        players.append(input())
         i += 1
-    time.sleep(0.5)
 
-print("ALRIGHT! Now let's start!")
-time.sleep(0.5)
+print("STARTING GAME...")
+time.sleep(1)
 
-print("It's your turn", (players[0])) # random library chooses a player
-time.sleep(0.5)
+roundPlayer = players[0]
 
-dice = random.sample(cup, 3) #trying to make it choose 3 dice and return the value of their faces
-for DiceFace in dice:
-    for x in DiceFace:
+print("It's your turn", roundPlayer)
+time.sleep(1)
 
+# randomly selects 3 dice from the dice holder
+diceChoice = random.sample(cup, 3)
+print("Your dice are:", diceChoice)
+time.sleep(1)
 
-print(DiceFace)
+#
+for c in diceChoice:
+    if c == 'GreenDice':
+        side = random.choice('BSBGSB')
+    elif c == 'YellowDice':
+        side = random.choice('GSBGSB')
+    elif c == 'RedDice':
+        side = random.choice('GSGBSG')
 
-# toss the dice from the cup
+    print("Your sides are:", side)
+    time.sleep(1)
+
+    while side == 'B':
+        brains = brains + 1
+        break
+    while side == 'G':
+        hearts = hearts - 1
+        break
+    while side == 'S':
+        steps = steps + 1
+        break
+
+print("You've eaten:", brains, "brains!")
+time.sleep(1)
+
+print(steps, "of your victims ran away...")
+time.sleep(1)
+
+print("You still have", hearts, "hearts!")
+time.sleep(1)
+
+print("ROUND 1 SCORE:")
+time.sleep(1)
+
+print("BRAINS:", brains)
+time.sleep(1)
+
+print("HEARTS:", hearts)
+time.sleep(1)
